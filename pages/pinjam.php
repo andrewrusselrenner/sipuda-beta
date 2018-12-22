b3c1585b78ead789800739ad02c06d8555aec083
andrewrusselrenner <talk.windows.inblog@live.com>
2018-12-22 08:27:13 +0800

    Modified some script

---
+++ pages/pinjam.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="pinjamModal"> <!-- nanti diganti idnya-->
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-header">
+             <h4 class="modal-title"> Sukses</h4>
+             <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <div class="modal-body text-center p-3">
+            <p class="py-2"><span class="fa fa-check-circle fa-4x"></span></p>
+            <h1> Permintaan Peminjaman Sukses! </h1>
+            <p>Silahkan datang ke perpustakaan dengan nomor permintaan dibawah ini untuk mengambil buku</p>
+            <br />
+            <p><h2>6509</h2></p>
+            </div>
+            
+            <div class="modal-footer">
+            <button tupe="button" class=" btn btn-danger" data-dismiss="modal"> Tutup </button>
+
+            </div>
+        </div>
+    </div>
+</div>
