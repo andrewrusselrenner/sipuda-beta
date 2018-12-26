78ec3842fef23d49594608ea996eb97c585796a6
andrewrusselrenner <talk.windows.inblog@live.com>
2018-12-26 10:46:40 +0800

    Make all js async

--- pages/script.php
+++ pages/script.php
@@ -1,14 +1,15 @@
 <!-- disini kumpulan script js -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!--<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous" async></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous" async></script>
 <!-- Material design js -->
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <!-- Masonry JS -->
 <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
 <!-- Script: Smooth scrolling between anchors in the same page -->
-<script src="../vendor/js/smooth-scroll.js"></script>
-<script src="../vendor/js/jquery-easing/jquery.easing.min.js"></script>
+<script async src="../vendor/js/smooth-scroll.js"></script>
+<script async src="../vendor/js/jquery-easing/jquery.easing.min.js"></script>
 <!--<script src="../vendor/js/sipudaCarousel.js"></script>-->
-<script src="../vendor/js/sipuda.js"></script>
-<script src="../vendor/js/sipuda-animelogin.js"></script>
+<script async src="../vendor/js/sipuda.js"></script>
+<script async src="../vendor/js/sipuda-animelogin.js"></script>
