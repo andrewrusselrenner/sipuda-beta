5cfa357d69cf7fd8a5b6037d9616baa5d0750a34
andrewrusselrenner <talk.windows.inblog@live.com>
2018-12-28 22:19:48 +0800

    Make a header bg as a book cover

--- pages/viewBook.php
+++ pages/viewBook.php
@@ -18,7 +18,7 @@ include('header.php');
 include('navbar.php');
 ?>
 <body>
-  <div class="py-5 text-center section-parallax" style="background-image: url(&quot;http://hdqwalls.com/wallpapers/christopher-robin-2018-movie-poster-3r.jpg&quot;);">
+  <div class="py-5 text-center section-parallax" style="background-image: url(&quot;<?php echo $baris['gambar'] ?>&quot;);">
     <div class="container-fluid">
       <div class="row my-3 mx-auto col-md-6 p-3 section-lighttwo">
         <div class="mx-auto col-md-10 p-3">
